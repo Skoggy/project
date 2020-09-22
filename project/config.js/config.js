b08f32d23f0eb7370b53aa40685a56d408cde335
Christopher Skogstad <Chris.skogstad1@gmail.com>
2020-09-22 15:48:54 +1000

    Delete config.js
    looks good, I removed the config.js as it is not needed

--- project/config.js/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-    googlemaps : 'AIzaSyCisxdhsD1_i5Pao60DV8Zaa818FtpryyQ',
-    weather : '8c321cc1716884b0a6eec6410a70fa25',
-    currency : '077422a8ec047bea40fab6ea',
-    covid : ''
-  }
